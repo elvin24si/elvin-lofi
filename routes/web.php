f6275ee17f5a8684b7af4e0629208144b728f094
auliatie <aulia23ti@mahasiswa.pcr.ac.id>
2025-09-22 08:00:17 +0700

    Commit pertama yang pernah saya lakukan

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatakuliahController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+
+Route::get('/mahasiswa', function () {
+    return 'Halo Mahasiswa';
+})->name('mahasiswa.show');
+
+Route::get(
+    '/mahasiswa/{param1?}', [MahasiswaController::class, 'show']
+)->name('mahasiswa.show');
+
+Route::get(
+    '/matakuliah/show/{param1?}', [MatakuliahController::class, 'show']
+)->name('matakuliah.show');
+
+Route::get('/nama/{param1?}', function ($param1) {
+    return 'Nama saya: ' . $param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: ' . $param1;
+});
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
