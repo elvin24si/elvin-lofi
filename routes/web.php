4b41e4ea417f76848d11c09a687991e1a741130d
Elvin Winata <elvin24si@mahasiswa.pcr.ac.id>
2025-10-06 08:24:01 +0700

    Install template Volt

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\MatakuliahController;
-use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -47,3 +48,6 @@ Route::post('/question/store', [QuestionController::class, 'store'])->name('ques
 Route::get('/auth', [AuthController::class, 'index']);
 Route::post('auth/login', [AuthController::class, 'login'])
     ->name('auth.login');
+
+Route::get('dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard');
